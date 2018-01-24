d1e5799414626b2f0cd1465e7d2e666ce33a97c3
C'Aira Shields <cairacshields@gmail.com>
2018-01-24 12:46:13 -0600

    add original files

---
+++ app.js
@@ -0,0 +1,33 @@
+var express        = require("express"),
+	bodyParser     = require("body-parser"),
+	mongoose       = require("mongoose"),
+	methodOverride = require("method-override"),
+	flash          = require("connect-flash"),
+	app            = express();
+
+//APP CONGIF
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function(req, res){
+	res.redirect("/index");
+});
+
+app.get("/index", function(req, res){
+	res.render("home");
+});
+
+
+
+app.get("*", function(req, res){
+	res.send("There is nothing on this page.");
+});
+
+app.listen(2000, function(){
+	console.log("The portfolio is coming along swell!");
+});
